71d89932467b5596649af0635438d069c466defb
mrLuisFer <mrluisfeer@gmail.com>
2021-09-15 09:55:32 -0500

    [feat] add motion.div for test

--- src/components/AnimationFramer/index.jsx
+++ src/components/AnimationFramer/index.jsx
@@ -1,3 +1,19 @@
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+
 export default function AnimationFramer() {
-  return <div></div>
+  const [isVisible, setIsVisible] = useState(false)
+  return (
+    <div>
+      {isVisible && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <p>Text</p>
+        </motion.div>
+      )}
+    </div>
+  )
 }
