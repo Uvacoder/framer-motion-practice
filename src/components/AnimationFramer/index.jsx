13fd163df6e8e886c58e36740ec5dff920182361
mrLuisFer <mrluisfeer@gmail.com>
2021-09-15 19:10:16 -0500

    [feat] animation details added

--- src/components/AnimationFramer/index.jsx
+++ src/components/AnimationFramer/index.jsx
@@ -1,19 +1,22 @@
-import { motion } from 'framer-motion'
 import { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export default function AnimationFramer() {
   const [isVisible, setIsVisible] = useState(false)
   return (
     <div>
-      {isVisible && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <p>Text</p>
-        </motion.div>
-      )}
+      <button onClick={() => setIsVisible((prev) => !prev)}>show</button>
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ opacity: 0, x: -100 }}
+          >
+            <p>Animated Text</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
