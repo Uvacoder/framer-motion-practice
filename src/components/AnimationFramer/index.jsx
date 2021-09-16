0a34cc6cdf2d3335146482a016a5e76bb05a57ef
mrLuisFer <mrluisfeer@gmail.com>
2021-09-15 19:47:32 -0500

    [feat] playing with motion.div

--- src/components/AnimationFramer/index.jsx
+++ src/components/AnimationFramer/index.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
+import styles from './styles.module.css'
 
 export default function AnimationFramer() {
   const [isVisible, setIsVisible] = useState(false)
+  const [showSecondBtn, setShowSecondBtn] = useState(false)
+
   return (
     <div>
-      <button onClick={() => setIsVisible((prev) => !prev)}>show</button>
+      <button
+        onClick={() => setIsVisible((prev) => !prev)}
+        className={styles.button}
+      >
+        show
+      </button>
       <AnimatePresence>
         {isVisible && (
           <motion.div
@@ -13,7 +21,25 @@ export default function AnimationFramer() {
             animate={{ x: 0, opacity: 1 }}
             exit={{ opacity: 0, x: -100 }}
           >
-            <p>Animated Text</p>
+            <div
+              className={styles.card}
+              onClick={() => setShowSecondBtn((prev) => !prev)}
+            >
+              <p>Animated Text</p>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {showSecondBtn && (
+          <motion.div
+            initial={{ opacity: 0, x: -200 }}
+            animate={{ x: 100, opacity: 1 }}
+            exit={{ opacity: 0, x: -200 }}
+          >
+            <div className={styles.card}>
+              <p>Second Text</p>
+            </div>
           </motion.div>
         )}
       </AnimatePresence>
