4dcbbfa3619e4aeb0bf552cbb0ba2b0aac3189ca
mrLuisFer <mrluisfeer@gmail.com>
2021-09-15 20:19:25 -0500

    [feat] add some details and effects

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <AnimationFramer />
-      <hr />
+      <Hr />
       <ButtonFramer />
     </div>
   )
--- src/components/ButtonFramer/index.jsx
+++ src/components/ButtonFramer/index.jsx
@@ -1,6 +1,9 @@
 import { motion } from 'framer-motion'
+import { useState } from 'react'
+import styles from './styles.module.css'
 
 export default function ButtonFramer() {
+  const [isHovered, setIsHovered] = useState(false)
   return (
     <div>
       <motion.button
@@ -9,9 +12,19 @@ export default function ButtonFramer() {
           transition: { duration: 0.2 },
         }}
         whileTap={{ scale: 0.9 }}
+        className={styles.button}
       >
         Button Framer
       </motion.button>
+      <motion.div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        animate={{ stdDeviation: isHovered ? 0 : 2, x: isHovered ? 30 : 0 }}
+        whileTap={{ y: 10 }}
+        className={styles.div}
+      >
+        Hovered Div
+      </motion.div>
     </div>
   )
 }
